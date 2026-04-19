5562e38145bc76b821d2c6a0897bc9f0a905ae2a
Dawood <dawoodsoomro56@gmail.com>
2026-04-20 03:49:51 +0500

    build: implemented Alembic migrations and removed create_all prototype code

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import tasks, auth
-from app.database import engine
 
 # Import models so SQLAlchemy knows they exist before creating tables
 from app.models import task as task_models
 from app.models import user as user_models
 
-# Syntax: Create Tables
-# Logic: Because we imported both task and user models above, SQLAlchemy
-# will physically build both the 'tasks' and 'users' tables in PostgreSQL.
-task_models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(title="Task Manager API")
 
